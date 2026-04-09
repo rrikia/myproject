8cee9d9d87cb3cdf9f05ab66746b4ff1e73c0488
rrikia <nguyenhango2006@gmail.com>
2026-04-09 12:06:36 +0200

    Add template and auth systems

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,5 +6,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     # add another url below
-    path('counter', views.counter, name='counter')
+    path('counter', views.counter, name='counter'), 
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('post/<str:pk>', views.post, name='post')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,35 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
 def index(request):
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Fast'
-    feature1.is_true = True
-    feature1.details = 'Our service is very quick'
-
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Reliable'
-    feature2.is_true = True
-    feature2.details = 'Our service is very reliable'
-
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Easy To Use'
-    feature3.is_true = False
-    feature3.details = 'Our service is easy to use'
-
-    feature4 = Feature()
-    feature4.id = 3
-    feature4.name = 'Affordable'
-    feature4.is_true = True
-    feature4.details = 'Our service is very affordable'
-
-    
-    features = [feature1, feature2, feature3, feature4]
+    features = Feature.objects.all()
 
     return render(request, 'index.html', {'features': features})
 
@@ -38,12 +15,55 @@ def index(request):
 # return render(request, 'index.html', {'name': name})
 # if u want multiple values, use a dictionary:
 
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        
+        # Password authentication 
+        if password == password2:  
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already Used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already Used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Password Not The Same')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        # We want to authenticate the user 
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect ('login')
+    return render (request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
 def counter(request):
-    text = request.POST['text']
-    # amount of words that the user input 
-    amount_of_words = len(text.split())
+    posts = [1, 2, 3, 4, 5, 'tim', 'tom', 'john']
+    return render(request, 'counter.html', {'posts': posts})
 
-    # send the amount of words to counter.html 
-    # use key value pair in the line below 
-    return render(request, 'counter.html', {'amount': amount_of_words})
-
+def post(request, pk):
+    return render(request, 'post.html', {'pk' : pk})
